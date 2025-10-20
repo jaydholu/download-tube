cc16e6407d8e2cdd1c1e40167ddaf9128d49dad1
Jay Dholu <jaydholu074@gmail.com>
2025-10-20 22:02:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,3 @@ apt-get update && apt-get install -y ffmpeg
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Run database creation (this will be run once on first build)
-# Note: For more complex apps, you would use migrations (e.g., Flask-Migrate)
-flask --app app shell <<< "from app import db; db.create_all()"
